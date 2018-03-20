f43a7396be96171a516bad5ab02967dda62f597f
Douglas Panhota <douglaspands@gmail.com>
2018-03-20 18:37:57 -0300

    Esqueleto do projeto

--- src/main/java/conversor/annotations/MainframeField.java
+++ src/main/java/conversor/annotations/MainframeField.java
@@ -2,10 +2,10 @@ package src.main.java.conversor.annotations;
 
 import java.lang.annotation.*;
 
-@Documented
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface MainframeField {
     int order() default 0;
     int length() default 0;
+    int decimal() default 0;
 }
